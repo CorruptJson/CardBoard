4a2c9c16e97fec4019a1fe1a116731e111789bbb
Jason Huang <jhuang242@my.bcit.ca>
2018-11-13 19:38:19 -0800

    Created test login page. Login not implemented

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const express = require('express')
 const auth = require('./scripts/auth.js')
+const hbs = require('hbs')
+const bodyParser = require('body-parser')
 
-//var session
+const app = express()
 
-var app = express()
+var session = undefined
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
+
 
 app.use(express.static(__dirname + '/public'))
 
 
-/*  IGNORE FOR NOW
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+
+app.get('/', (request, response) => {
+  response.render('login.hbs')
 })
 
 
-app.post('/login', (req, res) => {
-  console.log(req)
-  res.sendFile(__dirname + '/views/home.html')
+app.post('/login', (request, response) => {
+  response.send(request.body)
 })
 
 
-app.listen(8080, console.log('Server is up on the port 8080'))
-
-*/
 
 app.listen(8080, console.log('Server is up on the port 8080'))
