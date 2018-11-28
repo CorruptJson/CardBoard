fa4964d11c89645e74757f2a04db999bad34c362
Alex Bratlavskyy <abratslavskyy@my.bcit.ca>
2018-11-28 11:20:38 -0800

    made text input for create category

---
+++ public/js/new_card_inputs.js
@@ -0,0 +1,5 @@
+function createCat(){
+	document.getElementById("create_cat_add").style.display = "none";
+	document.getElementById("create_cat_text").style.display = "block";
+	document.getElementById("create_cat_submit").style.display = "block";
+}
